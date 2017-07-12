9a77d061c281c11290e37bbb3b62603a9db9f549
MatiasLarssonFI <matias.larsson@gmail.com>
2017-07-12 13:09:42 +0300

    Removed search from nav

--- header.php
+++ header.php
@@ -44,12 +44,6 @@
 							</button>
 						</div>
 						<?php activello_header_menu(); // main navigation ?>
-
-						<div class="nav-search"><?php
-							add_filter( 'get_search_form', 'activello_header_search_filter',10,3 );
-							echo get_search_form();
-							remove_filter( 'get_search_form', 'activello_header_search_filter' );?>							
-						</div>
 					</div>
 				</div>
 			</div>
