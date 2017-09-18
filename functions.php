0c86cf1f25d17e625b9e74b98cc7dcd67f5eb715
MatiasLarssonFI <matias.larsson@gmail.com>
2017-09-18 18:38:39 +0300

    Removed caption from slider

--- functions.php
+++ functions.php
@@ -97,10 +97,6 @@ if ( ! function_exists( 'activello_featured_slider' ) ) :
 						} else {
 							  echo get_the_post_thumbnail( get_the_ID(), 'activello-slider' );
 						}
-								echo '<div class="flex-caption">';
-						if ( get_the_title() != '' ) { echo '<a href="' . get_permalink() . '"><h2 class="entry-title">' . get_the_title() . '</h2></a>';
-						}
-								echo '</div>';
 								echo '</li>';
 						endif;
 					endwhile;
